9f9671e237ee713c9dfe97762f35dbe40df9c122
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-11-14 13:59:04 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import NotificationButton from "./components/notificationButton"
 
 
 function App() {
 
-  return(
-  <h1>Ola aluno</h1>)
+  return(<>
+  <h1>Ola aluno</h1>
+  <NotificationButton/>
+  </>
+  )
 }
 
 export default App
