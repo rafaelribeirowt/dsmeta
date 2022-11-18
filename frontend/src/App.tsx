6ff020a4d86f93b20076373a0d510e52ccfaca69
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-11-18 17:44:46 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,13 @@ import NotificationButton from "./components/notificationButton";
 import Hearder from "./components/header";
 import SalesCard from "./components/salesCard";
 import './index.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (<>
+  <ToastContainer/>
     <Hearder />
     <main>
       <section id="sales">
